8d6838f3f0b6598109310cdcba49993e56d7b242
Mikołaj Krzywacz <mikolaj.krzywacz@gmail.com>
2022-12-11 15:27:29 +0100

    FINISHED TASK 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryPhotos = galleryItems;
+const gallery = document.querySelector(".gallery");
+
+// creating gallery
+galleryPhotos.forEach((el) => {
+  const galleryItem = document.createElement("div");
+  const galleryLink = document.createElement("a");
+  const galleryImage = document.createElement("img");
+  galleryItem.setAttribute("class", "gallery__item");
+  galleryLink.setAttribute("class", "gallery__link");
+  galleryLink.setAttribute("href", `${el.original}`);
+  galleryImage.setAttribute("class", "gallery__image");
+  galleryImage.setAttribute("src", `${el.preview}`);
+  galleryImage.setAttribute("data-source", `${el.original}`);
+  galleryImage.setAttribute("alt", `${el.description}`);
+
+  gallery.append(galleryItem);
+  galleryItem.append(galleryLink);
+  galleryLink.append(galleryImage);
+});
+
+gallery.addEventListener("click", onImageClick);
+
+function onImageClick(evt) {
+  preventDefaultAction(evt);
+
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src ="${evt.target.dataset.source}" width= 800 height = 600>`
+  );
+  instance.show();
+
+  gallery.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+}
+
+function preventDefaultAction(evt) {
+  evt.preventDefault();
+}
